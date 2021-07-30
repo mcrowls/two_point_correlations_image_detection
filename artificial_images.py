3e787f39ba9d03450a86bce6feefd4e487dc606b
mcrowls <>
2021-07-30 17:09:55 +0100

    Recgonising the ellipses and then carrying out the two-point statistics for the artificial images

--- artificial_images.py
+++ artificial_images.py
@@ -5,6 +5,10 @@ import random
 import math
 import pandas as pd
 from matplotlib.patches import Ellipse
+from skimage import io, filters, img_as_float
+from skimage.measure import find_contours, label, regionprops, regionprops_table
+import cv2
+from scipy import ndimage
 
 
 def ellipse_inequality(x, y, x0, y0, a, b, theta):
@@ -339,7 +343,7 @@ def varying_r(image, stat, csv, num, min_r, max_r, plotting=False, iterations=10
         ax = plt.gca()
         draw_ellipses(csv)
     height, width = image.shape
-    rs = np.linspace(min_r, max_r, num+1)
+    rs = np.linspace(min_r, max_r, num)
     for i in range(num):
         tot = 0
         for t in range(iterations):
@@ -361,6 +365,61 @@ def varying_r(image, stat, csv, num, min_r, max_r, plotting=False, iterations=10
     return
 
 
-# image = io.imread("Images/Manual_Microstructures_Gray_3/image0.png")
-# fiber_csv = pd.read_csv("fibres/fiber_locations_0.csv")
-# varying_r(image, two_point_cluster, fiber_csv, 100, 0, 100)
+def find_ellipses_in_image(image):
+    '''
+    From the artificially generated image, we are able to access the locations and
+    orientations of the ellipses. These are all added to a csv file which is saved
+    in the folder locations specified as strings by the user.
+    '''
+    height, width = image.shape
+    area = height*width
+
+    ret_white, thresh_white = cv2.threshold(image, 200, 0, cv2.THRESH_TOZERO)
+    ret_white, thresh_black = cv2.threshold(image, 200, 255, cv2.THRESH_OTSU)
+
+    white_mask = thresh_white == 255
+    black_mask = thresh_black != 255
+
+    s = [[1, 1, 1], [1, 1, 1], [1, 1, 1]]
+    white_label, white_black = ndimage.label(white_mask, structure=s)
+    black_label, num_black = ndimage.label(black_mask, structure=s)
+
+    white_clusters = regionprops(white_label)
+    black_clusters = regionprops(black_label)
+
+    #print(white_clusters)
+    black_area = 0
+    orientations_black = []
+    black_centroids = []
+    black_axis_lengths = []
+    locations1 = pd.DataFrame(columns=['centroidx', 'centroidy', 'minor_axis_length', 'major_axis_length', 'orientation'])
+    for k in range(np.size(black_clusters)):
+        black_area += black_clusters[k].area
+        orientations_black.append(black_clusters[k].orientation)
+        black_centroids.append(black_clusters[k].centroid)
+        black_axis_lengths.append([black_clusters[k].minor_axis_length, black_clusters[k].major_axis_length])
+        dataframe_to_add = pd.DataFrame([[black_centroids[k][0], black_centroids[k][1], black_axis_lengths[k][0], black_axis_lengths[k][1], orientations_black[k]]], columns=['centroidx', 'centroidy', 'minor_axis_length', 'major_axis_length', 'orientation'])
+        locations1 = pd.concat([locations1, dataframe_to_add], ignore_index=True)
+
+    white_area = 0
+    orientations_white = []
+    white_centroids = []
+    white_axis_lengths = []
+    locations2 = pd.DataFrame(columns=['centroidx', 'centroidy', 'minor_axis_length', 'major_axis_length', 'orientation'])
+    for j in range(np.size(white_clusters)):
+        white_area += white_clusters[j].area
+        orientations_white.append(white_clusters[j].orientation)
+        white_centroids.append(white_clusters[j].centroid)
+        white_axis_lengths.append([white_clusters[j].minor_axis_length, white_clusters[j].major_axis_length])
+        dataframe_to_add = pd.DataFrame([[white_centroids[j][0], white_centroids[j][1], white_axis_lengths[j][0], white_axis_lengths[j][1], orientations_white[j]]], columns=['centroidx', 'centroidy', 'minor_axis_length', 'major_axis_length', 'orientation'])
+        locations2 = pd.concat([locations2, dataframe_to_add], ignore_index=True)
+
+
+    # locations1.to_csv(path_1 '.csv')
+    # locations2.to_csv(path_2 + '.csv')
+    return locations1, locations2
+
+
+image = io.imread("Manual_Microstructures/image0.png")
+fiber_csv, void_csv = find_ellipses_in_image(image)
+varying_r(image, two_point_cluster, fiber_csv, 100, 0, 100)
